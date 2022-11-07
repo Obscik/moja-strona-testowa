0d351ae50abc0177527fd72759036d5539a22617
Chomik Bodzio <38500332+Obscik@users.noreply.github.com>
2022-11-07 10:54:24 +0100

    test

---
+++ main.js
@@ -0,0 +1,7 @@
+function unfold()
+{
+    document.getElementById('row').classList.add("d-none")
+    document.getElementById('events').classList.remove("d-none")
+    document.getElementById('events').classList.add("d-flex")
+
+}
