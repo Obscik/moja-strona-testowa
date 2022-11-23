0853ce016b6277f2f79ea5137d7fc0bfe74b1634
Chomik Bodzio <38500332+Obscik@users.noreply.github.com>
2022-11-23 08:20:27 +0100

    test

---
+++ README.md
@@ -0,0 +1,2 @@
+## Acknowledgment
+- I am not a web developer. All the credit for this website goes to [Ram](https://github.com/rampatra).
--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-function unfold()
+function wydarzenia()
 {
     document.getElementById('row').classList.add("d-none")
     document.getElementById('events').classList.remove("d-none")
     document.getElementById('events').classList.add("d-flex")
+
+}
+
+function inne()
+{
+    document.getElementById('row').classList.add("d-none")
+    document.getElementById('inne').classList.remove("d-none")
+    document.getElementById('inne').classList.add("d-flex")
+
+}
+
+function przyrodnicze()
+{
+    document.getElementById('row').classList.add("d-none")
+    document.getElementById('przyrodnicze').classList.remove("d-none")
+    document.getElementById('przyrodnicze').classList.add("d-flex")
 
 }
